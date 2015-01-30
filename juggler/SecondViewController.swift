03655a4606d24ee115691302b3c4504a9e2e747d
Nico Hämäläinen <nico@sizeof.io>
2015-01-30 14:29:06 +0200

    Hello world

---
+++ juggler/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  juggler
+//
+//  Created by Nico Hämäläinen on 29/01/15.
+//  Copyright (c) 2015 Nico Hämäläinen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ jugglerTests/jugglerTests.swift
@@ -0,0 +1,34 @@
+//
+//  jugglerTests.swift
+//  jugglerTests
+//
+//  Created by Nico Hämäläinen on 29/01/15.
+//  Copyright (c) 2015 Nico Hämäläinen. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class jugglerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
